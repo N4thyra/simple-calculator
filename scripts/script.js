c2e5627458bdc977b607b95c600e866dd5da9124
Jiri Laza <N4thyra@users.noreply.github.com>
2017-07-19 19:35:54 +0200

    fixed a bug on android

--- scripts/script.js
+++ scripts/script.js
@@ -1,104 +1,191 @@
-// $(document).ready(function() {
-  const input = $('#screen'),
-        numbers = $('.nums'),
-        operators = $('.ops'),
-        result = $('#result'),
-        clear = $('#clear'),
-        log = $('#log'),
-        reset = $('#reset'),
-        error = $('#error'),
-        arrow = $('#arrow');
+$('document').ready(function() {
 
-  let   isDisplayed = false;
+  const display = $('#display'),
+        history = $('#history'),
+        errHandler = $('#error'),
+        symbol = ['+', '-', '÷', '×'];
 
-  numbers.on('click', function() {
-    let string = input.text();
-    let button = $(this).text();
+  let first = [],
+      second = [],
+      value = '',
+      result = [],
+      data = [],
+      operator = '',
+      a = 0,
+      b = 0,
+      isClicked;
 
-    app.insert(button);
-  });
+  function calc() {
 
-  operators.on('click', function() {
-    let string = input.text();
-    let button = $(this).text();
-    let lastChar = string[string.length - 1];
+    // selecting the right element
+      $('h3').on('click', (e) => {
 
-    if(lastChar === '×' || lastChar === '÷' || lastChar === '+' || lastChar === '-' || lastChar === '.') {
-      let newString = string.substring(0, string.length - 1) + button;
-      input.text(newString);
-      isDisplayed = false;
-    } else {
-      app.insert(button);
-      isDisplayed = false;
-      console.log(button);
+        errHandler.html('');
 
-    }
-  });
+        isClicked = false;
+        // get the value of a current element, and store it in variable
+        value = e.target.innerHTML;
 
-  result.on('click', function() {
-    if(!isDisplayed) {
-      let text = input.text();
-      let result = app.calculate(text);
+        //***FOR TESTING PURPOSES***//
+        console.log(operator);
+        console.log('value is: ', value);
+        //***FOR TESTING PURPOSES***//
 
-      if(result !== undefined) {
-      app.printResult(result);
-      app.save(text, result);
-      }
-    }
-    isDisplayed = true;
-  });
+        // loop through the array(symbol) and assing a value to the particular operator
+        loopThroughSymbols();
+        //handle possible errors
+        errorHandlers();
+        // check for a math operator and push a value into an array
+        checkAndAssign();
 
-  arrow.on('click', function() {
-    let string = input.text();
-    let newString = string.slice(0, string.length - 1);
+        //***FOR TESTING PURPOSES***//
+        console.log('first array: ', first);
+        console.log('result: ', result);
+        console.log('second array: ', second);
+        console.log('operator is: ', operator);
+        console.log('isClicked: ', isClicked);
+        //***FOR TESTING PURPOSES***//
 
-    input.text(newString);
-  });
+        // converting arrays into numbers
+        a = Number(first.join(''));
+        b = Number(second.join(''));
 
-  reset.on('click', function() {
-    app.reset();
-  });
+        // you have a value and an operator, choose what math operation to do
+        calculate();
 
-  clear.on('click', function() {
-    app.clear();
-  });
+        // show data on display
+        history.html(showHistory(data));
 
-  const app = {
-    insert(string) {
-      input.append(string);
-    },
-    reset() {
-      isDisplayed = false;
-      input.text('');
-      log.text('');
-    },
-    clear() {
-      isDisplayed = false;
-      input.text('');
-    },
-    calculate(result) {
-      let clean = result.replace(/\×/g, '*');
-          clean = clean.replace(/\÷/g, '/');
-          clean = clean.replace(/^\./g, '0.');
-      if(result === '' || clean === '*' || clean === '/' || result === '+' || result === '-') {
-        log.prepend('<p class="inside-box">Invalid Syntax</p>');
-        this.clear();
-      } else {
-          try {
-          clean = eval(clean);
-          let floored = Math.floor(clean * 1000000000) / 1000000000;
-          return floored;
-        } catch (e) {
-          log.prepend('<p class="inside-box">Invalid Syntax</p>');
-          this.clear();
-        }
-    }
-    },
-    printResult(result) {
-      input.text(result);
-    },
-    save(info, result) {
-      log.prepend(`<p class="inside-box">${info} = ${result}</p>`);
-    }
+        // check if CE was clicked  -- if so, set everything to default
+        ceClicked();
+      });
   }
-// });
+
+      // set everything to default values
+      function clearScreen() {
+        display.html('');
+        history.html('');
+        first = [];
+        second = [];
+        result = [];
+        data = [];
+        operator = '';
+        value = null;
+        a = 0;
+        b = 0;
+        console.log('-------------');
+        console.log(first, second, result, value, operator);
+      }
+
+      function setUp() {
+        display.html(result);
+        first = [result];
+        second = [];
+      }
+
+      function loopThroughSymbols() {
+        for(let i = 0; i < symbol.length; i++ ) {
+          if(value === symbol[i]) {
+            operator = value;
+            isClicked = true;
+            console.log('operator is: ', operator);
+            break;
+          }
+        }
+      }
+
+      function errorHandlers() {
+        if(isNaN(first) && isNaN(result) ||
+        first[0] === Infinity && result === Infinity ||
+        first[0] === undefined && operator !== ''
+        ) {
+          clearScreen();
+          errHandler.html('Invalid Operation -- Restarting App');
+        }
+
+        if(first[0] === '' && result[0] === undefined) {
+          first.pop();
+        }
+      }
+
+      function checkAndAssign() {
+        // saving a value into an array
+        if(!operator && value !== '=') {
+          first.push(value);
+          display.html(first);
+        } else if(operator && value !== '=') {
+            // if you haven't clicked on a math operator, show a number on the display
+          if(value !== '+' && value !== '-' && value !== '÷' && value !== '×' ) {
+            second.push(value);
+            display.html(second);
+          } else {
+            // else show an operator on the display
+            display.html(value);
+          }
+        }
+      }
+
+      function calculate() {
+        if(value === '=' || isClicked && second[0] !== undefined) {
+          if(operator === '+') {
+            result = add(a, b);
+            createData();
+            setUp();
+          }
+          else if(operator === '-') {
+            result = subtract(a, b);
+            createData();
+            setUp();
+          }
+          else if(operator === '÷') {
+            result = divide(a, b);
+            createData();
+            setUp();
+          }
+          else if(operator === '×') {
+            result = multiply(a, b);
+            createData();
+            setUp();
+          }
+        }
+      }
+
+      function ceClicked() {
+        if(value === 'CE') {
+          clearScreen();
+        }
+      }
+
+      // a function which makes a string out of first array, operator, second array, and the result
+      function createData() {
+        return data.push(`${a} ${operator} ${b} = ${result}`);
+      }
+
+      function showHistory(data) {
+        let output = '';
+        for(let i = 0; i < data.length; i++) {
+          output += `<p class='inside-box'>${data[i]}</p>`;
+        }
+        return output;
+      }
+
+      // math operations
+      function add(a, b) {
+        return a + b;
+      }
+
+      function subtract(a, b) {
+        return a - b;
+      }
+
+      function multiply(a, b) {
+        return a * b;
+      }
+
+      function divide(a, b) {
+        return a / b;
+      }
+
+      // init app
+      calc();
+  });
