b9197eb7396d17d358b02c45cacbc25330b5424f
Jiri Laza <N4thyra@users.noreply.github.com>
2017-07-20 08:56:36 +0200

    fixed a bug with adding multiple dots

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,34 @@
-// $(document).ready(function() {
-  const input = $('#screen'),
-        numbers = $('.nums'),
-        operators = $('.ops'),
-        result = $('#result'),
-        clear = $('#clear'),
-        log = $('#log'),
-        reset = $('#reset'),
-        error = $('#error'),
-        arrow = $('#arrow');
+const input = $('#screen'),
+      numbers = $('.nums'),
+      operators = $('.ops'),
+      result = $('#result'),
+      clear = $('#clear'),
+      log = $('#log'),
+      reset = $('#reset'),
+      error = $('#error'),
+      arrow = $('#arrow'),
+      dot = $('#dot');
 
-  let   isDisplayed = false;
+let   isDisplayed = false;
 
-  numbers.on('click', function() {
-    let string = input.text();
-    let button = $(this).text();
+numbers.on('click', function() {
+  if(isDisplayed) {
+    input.text('');
+    isDisplayed = false;
+  }
+  let string = input.text();
+  let button = $(this).text();
 
-    app.insert(button);
-  });
+  app.insert(button);
+});
 
-  operators.on('click', function() {
-    let string = input.text();
-    let button = $(this).text();
-    let lastChar = string[string.length - 1];
+operators.on('click', function() {
+  let string = input.text();
+  let button = $(this).text();
+  let lastChar = string[string.length - 1];
+  let isDot = false;
 
-    if(lastChar === '×' || lastChar === '÷' || lastChar === '+' || lastChar === '-' || lastChar === '.') {
+  if(lastChar === '×' || lastChar === '÷' || lastChar === '+' || lastChar === '-' || lastChar === '.') {
       let newString = string.substring(0, string.length - 1) + button;
       input.text(newString);
       isDisplayed = false;
@@ -31,74 +36,100 @@
       app.insert(button);
       isDisplayed = false;
       console.log(button);
-
     }
-  });
+});
 
-  result.on('click', function() {
-    if(!isDisplayed) {
-      let text = input.text();
-      let result = app.calculate(text);
+dot.on('click', function() {
+  input.append('.');
+  let string = input.text();
+  let counter = 0;
 
-      if(result !== undefined) {
-      app.printResult(result);
-      app.save(text, result);
-      }
+  let lastChar = string[string.length - 2];
+  console.log('last char is: ', lastChar);
+  if(lastChar === '×' || lastChar === '÷' || lastChar === '+' || lastChar === '-') {
+    // check if the previous char was an operator and if so, replace it
+      let newString = string.substring(0, string.length - 1);
+      input.text(newString);
     }
-    isDisplayed = true;
-  });
 
-  arrow.on('click', function() {
-    let string = input.text();
-    let newString = string.slice(0, string.length - 1);
-
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] === '.') counter++;
+    if(string[i] === '+') counter = 0;
+    if(string[i] === '*') counter = 0;
+    if(string[i] === '÷') counter = 0;
+    if(string[i] === '×') counter = 0;
+  }
+  if(counter === 1 || counter === 0) {
+    // check if there is a dot
+  } else {
+    let newString = string.substring(0, string.length - 1);
     input.text(newString);
-  });
+  }
+});
 
-  reset.on('click', function() {
-    app.reset();
-  });
+result.on('click', function() {
+  if(!isDisplayed) {
+    let text = input.text();
+    let result = app.calculate(text);
 
-  clear.on('click', function() {
-    app.clear();
-  });
-
-  const app = {
-    insert(string) {
-      input.append(string);
-    },
-    reset() {
-      isDisplayed = false;
-      input.text('');
-      log.text('');
-    },
-    clear() {
-      isDisplayed = false;
-      input.text('');
-    },
-    calculate(result) {
-      let clean = result.replace(/\×/g, '*');
-          clean = clean.replace(/\÷/g, '/');
-          clean = clean.replace(/^\./g, '0.');
-      if(result === '' || clean === '*' || clean === '/' || result === '+' || result === '-') {
-        log.prepend('<p class="inside-box">Invalid Syntax</p>');
-        this.clear();
-      } else {
-          try {
-          clean = eval(clean);
-          let floored = Math.floor(clean * 1000000000) / 1000000000;
-          return floored;
-        } catch (e) {
-          log.prepend('<p class="inside-box">Invalid Syntax</p>');
-          this.clear();
-        }
-    }
-    },
-    printResult(result) {
-      input.text(result);
-    },
-    save(info, result) {
-      log.prepend(`<p class="inside-box">${info} = ${result}</p>`);
+    if(result !== undefined) {
+    app.printResult(result);
+    app.save(text, result);
     }
   }
-// });
+  isDisplayed = true;
+});
+
+arrow.on('click', function() {
+  let string = input.text();
+  let newString = string.slice(0, string.length - 1);
+
+  input.text(newString);
+});
+
+reset.on('click', function() {
+  app.reset();
+});
+
+clear.on('click', function() {
+  app.clear();
+});
+
+const app = {
+  insert(string) {
+    input.append(string);
+  },
+  reset() {
+    isDisplayed = false;
+    input.text('');
+    log.text('');
+  },
+  clear() {
+    isDisplayed = false;
+    input.text('');
+  },
+  calculate(result) {
+    let clean = result.replace(/\×/g, '*');
+        clean = clean.replace(/\÷/g, '/');
+        clean = clean.replace(/^\./g, '0.');
+    if(result === '' || clean === '*' || clean === '/' || result === '+' || result === '-') {
+      log.prepend('<p class="inside-box">Invalid Syntax</p>');
+      this.clear();
+    } else {
+        try {
+        clean = eval(clean);
+        let floored = Math.floor(clean * 1000000000) / 1000000000;
+        return floored;
+      } catch (e) {
+        log.prepend('<p class="inside-box">Invalid Syntax</p>');
+        this.clear();
+      }
+  }
+  },
+  printResult(result) {
+    input.text(result);
+  },
+  save(info, result) {
+    log.prepend(`<p class="inside-box">${info} = ${result}</p>`);
+  }
+}
